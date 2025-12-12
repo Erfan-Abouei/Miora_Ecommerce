b64a0d806cc62c106cfd1323a5e5ffbfb21d1502
Erfan-Abouei <erfangamer138787@gmail.com>
2025-12-12 16:08:16 +0330

    ref: change cors option

--- src/config/cors/cors.config.ts
+++ src/config/cors/cors.config.ts
@@ -1,7 +1,7 @@
 import { CorsOptions } from 'cors';
 
 export const corsOptions: CorsOptions = {
-  origin: '*', //['https://miora.karaflow.com', 'http://localhost:5173'] for production,
+  origin: 'http://localhost:8000', //['https://miora.karaflow.com', 'http://localhost:5173'] for production,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true,
 };
