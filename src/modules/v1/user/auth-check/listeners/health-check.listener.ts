208c1aedc8a667582a740c7d9f9fe504e5affa20
Erfan-Abouei <erfangamer138787@gmail.com>
2025-12-12 17:06:08 +0330

    fix: user auth check event name

--- src/modules/v1/user/auth-check/listeners/health-check.listener.ts
+++ src/modules/v1/user/auth-check/listeners/health-check.listener.ts
@@ -2,6 +2,6 @@ import { eventEmitter } from '@/config/emitter/event-emitter.config';
 import { UserEventName } from '@/constants/events/USER_EVENTS.constants';
 import { TokenPayload } from '@/types/common/basic.type';
 
-eventEmitter.on(UserEventName.USER_LOGIN_STATUS, (token: TokenPayload) => {
+eventEmitter.on(UserEventName.USER_AUTH_CHECK, (token: TokenPayload) => {
   // request for login status notification !
 });
