fa8e1d6922b0fca0cef0ad12372dd58761096b72
Erfan-Abouei <erfangamer138787@gmail.com>
2025-12-13 13:15:20 +0330

    change: otp code to 5 number

--- src/modules/v1/public/register-user/repositories/register-user.repository.ts
+++ src/modules/v1/public/register-user/repositories/register-user.repository.ts
@@ -12,7 +12,7 @@ export const registerUserRepository = async ({ email, password, phone_number }: 
   if (existingOtp !== undefined) {
     const otpTtl: number = cache.getTtl(`otp:${phone_number}`)!;
     const now = Date.now();
-    const expire_otp_timer = Math.floor((otpTtl - now) / 1000); // convert to second
+    const expire_otp_timer = Math.floor((otpTtl - now) / 10000); // convert to second
 
     return {
       expire_otp_timer,
